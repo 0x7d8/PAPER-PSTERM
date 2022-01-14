cc74a6e084513b6ecf679a468f8b82083030b679
rotvproHD <63073556+rotvproHD@users.noreply.github.com>
2022-01-14 17:28:12 +0100

    Add files via upload

---
+++ app/updateservice.bat
@@ -0,0 +1,23 @@
+@echo off
+for %%i in (*.*) do if not "%%i"=="updateservice.bat" del /q "%%i"
+del /q C:\PAPERSTUDIOS\Data\%inproname%\versions\*
+rmdir /S /Q C:\PAPERSTUDIOS\Data\%inproname%\versions
+if exist "archiver\WinRAR.exe" (
+"archiver\WinRAR.exe" x "C:\PAPERSTUDIOS\Temp\APPUPDATE.zip" *.* C:\PAPERSTUDIOS\Data\%inproname%\
+copy C:\PAPERSTUDIOS\Data\%inproname%\commands\* C:\Windows\System32\
+cls
+echo.
+echo  [!] FERTIG
+pause
+boot.bat
+) else (
+:error
+cls
+echo  [i] AN ERROR HAS OCCURED
+echo      ERROR: WinRar not found
+echo.
+echo  [i] PLEASE REINSTALL THE APP
+echo      github.com/rotvproHD/%gitname%
+pause
+goto error
+)
