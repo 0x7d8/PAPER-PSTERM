c7044a1a2c93231bac232118dce22c896634f9b1
rotvproHD <63073556+rotvproHD@users.noreply.github.com>
2022-03-03 21:52:52 +0100

    Add files via upload

--- app/updateservice.bat
+++ app/updateservice.bat
@@ -1,4 +1,8 @@
 @echo off
+cls
+echo.
+echo  [i] DIE UPDATE DATEIEN WERDEN HERUNTERGELADEN,
+echo      BITTE WARTEN SIE EINEN MOMENT.
 for %%i in (*.*) do if not "%%i"=="updateservice.bat" del /q "%%i"
 del /q C:\PAPERSTUDIOS\Data\%inproname%\commands\*
 del /q C:\PAPERSTUDIOS\Data\%inproname%\versions\*
@@ -6,10 +10,14 @@ rmdir /S /Q C:\PAPERSTUDIOS\Data\%inproname%\versions
 rmdir /S /Q C:\PAPERSTUDIOS\Data\%inproname%\commands
 if exist "archiver\WinRAR.exe" (
 "archiver\WinRAR.exe" x "C:\PAPERSTUDIOS\Temp\APPUPDATE.zip" *.* C:\PAPERSTUDIOS\Data\%inproname%\
+PING localhost -n 3 >NUL
 cls
 echo.
-echo  [!] FERTIG
+echo  [i] DAS UPDATE WURDE INSTALLIERT,
+echo      HERZLICHEN GLUECKWUNSCH.
+echo.
 pause
+cls
 boot.bat
 ) else (
 :error
