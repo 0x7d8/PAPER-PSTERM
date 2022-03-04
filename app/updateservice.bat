f84c8c33aa66989bec546d0f059f74acd5193e14
rotvproHD <63073556+rotvproHD@users.noreply.github.com>
2022-03-04 07:46:50 +0100

    Add files via upload

--- app/updateservice.bat
+++ app/updateservice.bat
@@ -3,13 +3,29 @@ cls
 echo.
 echo  [i] DIE UPDATE DATEIEN WERDEN HERUNTERGELADEN,
 echo      BITTE WARTEN SIE EINEN MOMENT.
+PING localhost -n 3 >NUL
+call internet.cmd https://github.com/rotvproHD/PAPER-PSTERM/raw/main/app/APPFILES.zip -saveTo C:\PAPERSTUDIOS\Temp\APPUPDATE.zip
 for %%i in (*.*) do if not "%%i"=="updateservice.bat" del /q "%%i"
+cls
+echo.
+echo  [i] ES IST AM BESTEN, WENN SIE IMMER DIE
+echo      NEUSTE VERSION UNSERER SOFTWARE BESITZEN
+PING localhost -n 3 >NUL
 del /q C:\PAPERSTUDIOS\Data\%inproname%\commands\*
 del /q C:\PAPERSTUDIOS\Data\%inproname%\versions\*
 rmdir /S /Q C:\PAPERSTUDIOS\Data\%inproname%\versions
 rmdir /S /Q C:\PAPERSTUDIOS\Data\%inproname%\commands
+cls
+echo.
+echo  [i] FALLS FEHLER IN DER NEUEN VERSION AUFTRETEN,
+echo      KOENNEN SIE DIE IN UPAAI UNTER FORMS MELDEN
+PING localhost -n 3 >NUL
 if exist "archiver\WinRAR.exe" (
 "archiver\WinRAR.exe" x "C:\PAPERSTUDIOS\Temp\APPUPDATE.zip" *.* C:\PAPERSTUDIOS\Data\%inproname%\
+cls
+echo.
+echo  [i] DAS UPDATE IST FAST FERTIG INSTALLIERT,
+echo      NUR NOCH WENIGE MOMENTE
 PING localhost -n 3 >NUL
 cls
 echo.
